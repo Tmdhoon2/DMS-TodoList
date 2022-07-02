863d6cedd5b0224d14a8aeee090b9dfb816f9900
Tmdhoon_2 <hks026naver@gmail.com>
2022-07-03 00:30:34 +0900

    Recyclerview

--- TodoList/app/src/main/java/com/tmdhoon/todolist/MainActivity.java
+++ TodoList/app/src/main/java/com/tmdhoon/todolist/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.tmdhoon.todolist.databinding.ActivityMainBinding;
 import com.tmdhoon.todolist.fragment.HomeFragment;
 import com.tmdhoon.todolist.fragment.MypageFragment;
@@ -17,7 +18,6 @@ import com.tmdhoon.todolist.fragment.SearchFragment;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-
     private FragmentManager fragmentManager = getSupportFragmentManager();
     private HomeFragment homeFragment = new HomeFragment();
     private SearchFragment searchFragment = new SearchFragment();
@@ -26,37 +26,34 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         FragmentTransaction transaction;
         transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.framelayout, homeFragment).commitAllowingStateLoss();
+        transaction.replace(R.id.framelayout, homeFragment).commitNowAllowingStateLoss();
 
-        binding.bottomnavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        binding.bottomnavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener(){
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            public boolean onNavigationItemSelected(@NonNull MenuItem item){
 
                 FragmentTransaction transaction = fragmentManager.beginTransaction();
 
                 int id = item.getItemId();
 
                 switch(id){
-
                     case R.id.menu_home:
                         transaction.replace(R.id.framelayout, homeFragment).commitAllowingStateLoss();
                         break;
 
                     case R.id.menu_search:
-                        transaction.replace(R.id.framelayout, homeFragment).commitAllowingStateLoss();
+                        transaction.replace(R.id.framelayout, searchFragment).commitAllowingStateLoss();
                         break;
 
                     case R.id.menu_mypage:
-                        transaction.replace(R.id.framelayout, homeFragment).commitAllowingStateLoss();
+                        transaction.replace(R.id.framelayout, mypageFragment).commitAllowingStateLoss();
                         break;
                 }
-
                 return true;
             }
         });
