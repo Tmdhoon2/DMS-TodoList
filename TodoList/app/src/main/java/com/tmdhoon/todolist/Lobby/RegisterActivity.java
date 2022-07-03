7c84643b35bd991da6678313d2ce15c8be84e3c9
Tmdhoon_2 <hks026naver@gmail.com>
2022-07-03 13:25:09 +0900

    GetText

--- TodoList/app/src/main/java/com/tmdhoon/todolist/Lobby/RegisterActivity.java
+++ TodoList/app/src/main/java/com/tmdhoon/todolist/Lobby/RegisterActivity.java
@@ -1,4 +1,4 @@
-package com.tmdhoon.todolist;
+package com.tmdhoon.todolist.Lobby;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,7 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+
+import com.tmdhoon.todolist.Api.ApiProvider;
+import com.tmdhoon.todolist.ApiMethod;
 import com.tmdhoon.todolist.databinding.ActivityRegisterBinding;
+
+import retrofit2.Retrofit;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -19,13 +24,20 @@ public class RegisterActivity extends AppCompatActivity {
         binding = ActivityRegisterBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.etRegisterName.getText().toString();
+        binding.etRegisterAge.getText();
+        binding.etRegisterId.getText().toString();
+        binding.etRegisterPw.getText().toString();
+
+        ApiMethod apiMethod = ApiProvider.getRetrofit().create(ApiMethod.class);
+
         binding.btRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(intent);
+                apiMethod
             }
         });
+
 
 
     }
